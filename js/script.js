435e7b2e90811709bc4b888bf83af5c61a0c9fb7
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-10-19 13:43:43 +0200

    start-repository

---
+++ js/script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click',
+function()
+{
+    const squareContainer = document.getElementById('grid_container');
+    // reset griglia al click
+    squareContainer.innerHTML = '';
+    // generare n numeri elementi
+    let difficulty = document.getElementById('difficulty').value;
+    let squareNumber = parseInt(difficulty);
+    for(let i = 1; i <= squareNumber; i ++)
+    {
+        let square = getSquare(i, squareNumber);
+        squareContainer.append(square)
+    }
+    
+});
+
+function getSquare(squareIndex, squareNumber)
+{
+    // creare elementi e applicare lo sitle
+    const square = document.createElement('div');
+    let squareHeight = Math.sqrt(squareNumber)
+
+    square.classList.add('square');
+    square.innerHTML = squareIndex;
+    square.style.height = `calc(100% / ${squareHeight})`
+
+    // cambio sfondo al click
+    square.addEventListener('click',
+    function()
+    {
+        square.classList.add('active');
+        console.log(squareIndex)
+    });
+    return square;
+};
